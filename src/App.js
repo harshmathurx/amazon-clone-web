f25ddd1d89dc2426ce63dde2ce788d6670a76187
harshmathurx <harshmathurx@gmail.com>
2021-08-08 15:39:25 +0530

    Initial commit // home route implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./Header.jsx";
 import './App.css';
-
+import Home from "./Home.jsx"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM naming convention
+    <Router>
+      <div className="app">
+        <Switch>
+          {/* <Route path="/checkout">
+            <Header />
+            <h1>I am here to checkout</h1>
+          </Route> */}
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
